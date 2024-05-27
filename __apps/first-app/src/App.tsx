df5cd725d5e2b9df103a0c3ee9a76844f9e358f1
Obodo David <obododavid5@gmail.com>
2024-05-27 05:37:27 +0100

    successfully run 2 independent react applications

---
+++ __apps/first-app/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1>THE FIRST APPLICATION</h1>
+		</>
+	);
+}
+
+export default App;
---
+++ __apps/second-app/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1>THE SECOND APPLICATION</h1>
+		</>
+	);
+}
+
+export default App;
