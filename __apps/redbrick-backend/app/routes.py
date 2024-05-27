2cb39a1cc1fd5b7425d2ace7b3b4af5e4b006d72
Obodo David <obododavid5@gmail.com>
2024-05-27 20:00:28 +0100

    add redbrick backend

--- __apps/redbrick-backend/app/routes.py
+++ __apps/redbrick-backend/app/routes.py
@@ -3,14 +3,14 @@ from flask import jsonify, request
 
 @app.route('/')
 def index():
-    return jsonify({"message": "Welcome to the simple AltaDB Server"})
+    return jsonify({"message": "Welcome to the simple Redbrick Server"})
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    data = {"data": "Here is some data from AltaDB Server"}
+    data = {"data": "Here is some data from Redbrick Server"}
     return jsonify(data)
 
 @app.route('/api/status', methods=['GET'])
 def get_status():
-    status = {"status": "Everything is running smoothly from AltaDB Server"}
+    status = {"status": "Everything is running smoothly from Redbrick Server"}
     return jsonify(status)
