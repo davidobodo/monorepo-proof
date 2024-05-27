9f1ed61d247a44460ef8aaf3405020aae3f9e7ff
Obodo David <obododavid5@gmail.com>
2024-05-27 23:07:38 +0100

    create sample ui package and use it in redbrick frontend

---
+++ __packages/ui/src/button/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button({ text, onClick }: { text: string; onClick: () => void }) {
+	return (
+		<button onClick={onClick} style={{ color: "red", backgroundColor: "white" }}>
+			{text}
+		</button>
+	);
+}
