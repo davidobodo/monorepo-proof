1ca609149eae90669e4a7cfb73900697fac98a46
Obodo David <obododavid5@gmail.com>
2024-06-10 09:57:07 +0100

    front(ui) - :zap: add disabled attribute to button

--- __packages/ui/src/button/Button.tsx
+++ __packages/ui/src/button/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button({ text, onClick }: { text: string; onClick: () => void }) {
 	return (
-		<button onClick={onClick} style={{ color: "red", backgroundColor: "white" }}>
+		<button onClick={onClick} style={{ color: "red", backgroundColor: "white" }} disabled={false}>
 			{text}
 		</button>
 	);
