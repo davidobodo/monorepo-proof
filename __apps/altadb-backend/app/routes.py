666a81373319e4812d67cd0db600ca71fcbae54d
Obodo David <obododavid5@gmail.com>
2024-06-10 10:03:36 +0100

    back(altadb) - :zap: add another backend text

--- __apps/altadb-backend/app/routes.py
+++ __apps/altadb-backend/app/routes.py
@@ -12,5 +12,5 @@ def get_data():
 
 @app.route('/api/status', methods=['GET'])
 def get_status():
-    status = {"status": "Everything is running smoothly from AltaDB Server1"}
+    status = {"status": "Everything is running smoothly from AltaDB Server12"}
     return jsonify(status)
