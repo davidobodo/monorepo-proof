81ea6d26bd4b8f0da72344c796fe19127910d6a8
Obodo David <obododavid5@gmail.com>
2024-06-12 10:54:42 +0100

    front(altadb) - :zap: add B

--- __apps/altadb-frontend/src/App.tsx
+++ __apps/altadb-frontend/src/App.tsx
@@ -45,7 +45,7 @@ function App() {
 				more again and more text again here and therezx zz and i am here abcdefgh sds abcdefgh fhjgd ok i an sdfsf zxcxzcjhj
 				today anaa sda 2345 asadbd]sdfsjkxzdsfdshjds
 			</p>
-			<p>a</p>
+			<p>ab</p>
 			<p>Add new paragraph</p>
 			<Button onClick={handleGetData} text="Get Data" />
 			<Button onClick={handleGetStatus} text="Get Status" />
